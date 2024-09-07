f4d87e8b10cc8d843944ee34efb9d4c0e09e6f66
Jessica Ilok <jilok@usf.edu>
2024-09-07 17:06:49 -0400

    changed the tip to correct data type

--- bill_splitter.js
+++ bill_splitter.js
@@ -1,14 +1,14 @@
 //Task 1- Calulate the tip
 
-let billAmount = 200; // Assuming a customer pays $200
+let billSize = 200; // Assuming a customer pays $200
 
-let tip;
-if (billAmount >= 50 && billAmount <= 300) {
-    tip = billAmount * 0.15;
+var tip;
+if (billSize >= 50 && billSize <= 300) {
+    tip = billSize * 0.15;
 } else {
-    tip = billAmount * 0.20;   //this is the ternary operator for the required scenario
+    tip = billSize * 0.20;   //this is the ternary operator for the required scenario
 }
  
 //Task 2- Output details
-console.log("The bill was $" + billAmount + ", the tip was $" + tip + ", and the total value is $" + (billAmount + tip));  //bill is $200, tip is $30 and total value is $230 in this case.
+console.log("The bill was $" + billSize + ", the tip was $" + tip + ", and the total value is $" + (billSize + tip));  //bill is $200, tip is $30 and total value is $230 in this case.
 
