3af67e7f63e639e360c173c865f7178b882b0c45
Jessica Ilok <jilok@usf.edu>
2024-09-07 19:09:41 -0400

    deleted entire code

--- bill_splitter.js
+++ bill_splitter.js
@@ -1,14 +1,3 @@
 //Task 1- Calulate the tip
 
-let billSize = 200; // Assuming a customer pays $200
-
-var tip;
-if (billSize >= 50 && billSize <= 300) {
-    tip = billSize * 0.15;
-} else {
-    tip = billSize * 0.20;   //this is the ternary operator for the required scenario
-}
- 
-//Task 2- Output details
-console.log("The bill was $" + billSize + ", the tip was $" + tip + ", and the total value is $" + (billSize + tip));  //bill is $200, tip is $30 and total value is $230 in this case.
-
+l
