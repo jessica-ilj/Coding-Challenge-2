3636b0d078cbd1ca0aad20dc5b1cb1ac7a5113b5
Jessica Ilok <jilok@usf.edu>
2024-09-07 21:09:02 -0400

    Initial logic for calculating tip using ternary operator

--- bill_splitter.js
+++ bill_splitter.js
@@ -1,3 +1,7 @@
 //Task 1- Calulate the tip
 
-l
+let billSize = 200; // Assuming a customer pays $200
+
+// Using a ternary operator to calculate the tip
+var tip = (billSize >= 50 && billSize <= 300) ? billSize * 0.15 : billSize * 0.2;
+
