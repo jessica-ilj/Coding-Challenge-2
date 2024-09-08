9c27ac4baba0adde9cd46ef2d0306f8a3f9de680
Jessica Ilok <jilok@usf.edu>
2024-09-07 21:24:34 -0400

    Add output functionality to display bill, tip, and total

--- bill_splitter.js
+++ bill_splitter.js
@@ -2,6 +2,8 @@
 
 let billSize = 200; // Assuming a customer pays $200
 
-// Using a ternary operator to calculate the tip
-var tip = (billSize >= 50 && billSize <= 300) ? billSize * 0.15 : billSize * 0.2;
+var tip = (billSize >= 50 && billSize <= 300) ? billSize * 0.15 : billSize * 0.2; // Using a ternary operator to calculate the tip
 
+
+//Task 2- Output Details
+console.log("The bill was $"+ billSize + ", the tip was $" + tip + ",and the total value is $" + (billSize+tip)); //in this case, The bill was $200, the tip was $30,and the total value is $230.
